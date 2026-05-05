a9de28362e6607e8d75eabb5865f9d7120b8e1d4
Vandana Chauhan Ajara <vandanac1609@gmail.com>
2026-05-05 11:37:34 +0530

    Git configuration error fix
    # Conflicts:
    #	Rental_inventory_management/settings.py

--- Rental_inventory_management/settings.py
+++ Rental_inventory_management/settings.py
@@ -1,21 +1,32 @@
+"""
+Django settings for Rental_inventory_management project.
+"""
+
 import os
 from pathlib import Path
-import dj_database_url
 import cloudinary
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
-DEBUG = os.environ.get("DEBUG", "False") == "True"
+SECRET_KEY = 'django-insecure-!u1(+cionufj+$8fdnd#)&c^!yncrrp3pt0w($+pr-elonhwfe'
+DEBUG = True
+ALLOWED_HOSTS = []
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", ".herokuapp.com"]
-
+# Cloudinary direct SDK config
 cloudinary.config(
-    cloud_name=os.environ.get("CLOUDINARY_CLOUD_NAME", "dvemtlkjh"),
-    api_key=os.environ.get("CLOUDINARY_API_KEY", ""),
-    api_secret=os.environ.get("CLOUDINARY_API_SECRET", ""),
+    cloud_name="dvemtlkjh",
+    api_key="679749273824336",
+    api_secret="t4LpyFrIjqUPJ2stsBvDwHbLcA0",
     secure=True,
 )
+
+# Optional: keep this only if you also use django-cloudinary-storage elsewhere
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': "dvemtlkjh",
+    'API_KEY': "679749273824336",
+    'API_SECRET': "t4LpyFrIjqUPJ2stsBvDwHbLcA0",
+}
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -25,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'inventory',
+    # optional, only if installed:
+    # 'cloudinary',
 ]
 
 MIDDLEWARE = [
@@ -56,13 +69,30 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Rental_inventory_management.wsgi.application'
 
-DATABASES = {
-    "default": dj_database_url.config(
-        default="postgresql://postgres:kvan@localhost:5432/rental_inventory_sound",
-        conn_max_age=600,
-        ssl_require=True,
-    )
-}
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if DATABASE_URL:
+    # Production (or when env var is set)
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=not DEBUG,
+        )
+    }
+else:
+    # Local development PostgreSQL
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "rental_inventory_sound",
+            "USER": "postgres",
+            "PASSWORD": "kvan",
+            "HOST": "localhost",
+            "PORT": "5432",
+        }
+    }
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -84,10 +114,7 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = '/static/'
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
